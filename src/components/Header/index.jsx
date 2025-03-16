df1d110558285f8ce8041532aa461c2c5a559deb
Zhm1603 <163318988+Zhm1603@users.noreply.github.com>
2025-03-16 21:32:28 +0500

    added files

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+import logo from "../../assets/Vector.png"
+
+const Header = () => {
+    return(
+        <header>
+            <img className="logo" src={logo}></img>
+            <nav className="nav-bar">
+
+
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
