6a1274111100ab615bd674be2478598d7d34b11d
Zhm1603 <zhanel.marat2006@gmail.com>
2025-04-27 16:00:59 +0600

    svg selector using

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,53 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router";
+import SvgSelector from "../../assets/SvgSelector/SvgSelector";
 import "./style.css";
-import logo from "../../assets/Logo.svg";
-import cube from "../../assets/cube.svg";
-import phoneBlured from "../../assets/phoneBlured.svg";
-import phone from "../../assets/phone.svg";
-import cubeBlured from "../../assets/cubeBlured.svg";
-
 
 const Header = () => {
   const navigatePages = useNavigate();
   const location = useLocation();
 
-  const [isChecked, setIsChecked] = useState(location.pathname === '/users');
+  const [isChecked, setIsChecked] = useState(location.pathname === "/users");
 
-  useEffect(()=>{
-    navigatePages(isChecked?"/users":"/posts");
-  },[isChecked,navigatePages])
- 
+  useEffect(() => {
+    navigatePages(isChecked ? "/users" : "/posts");
+  }, [isChecked, navigatePages]);
 
-    return(
-        <header className="header-vertical">
-            <img className="logo" src={logo} alt="Logo" />
-            <nav className="nav-bar">
-                <input
-                type="checkbox"
-                id="toggle"
-                checked={isChecked}
-                onChange={() => setIsChecked(!isChecked)}
-                className="toggle-switch"
-                />
-    
-                <label 
-                  htmlFor="toggle" 
-                  className="toggle-container"
-                  onMouseEnter={() => setIsChecked(!isChecked)}
-                >
-                  <span className="icon icon-left">
-                    <img src={isChecked ? cubeBlured : cube} alt="Cube" />
-                  </span>
-                  <div className={`toggle-dot ${isChecked ? "checked" : ""}`}></div>
-                  <span className="icon icon-right">
-                    <img src={isChecked ? phone : phoneBlured} alt="Phone" />
-                  </span>
-                </label>
-            </nav>
-        </header>
-    );
+  const handleToggleClick = () => {
+    setIsChecked((prev) => !prev);
+  };
+
+  return (
+    <header className="header-vertical">
+      <SvgSelector className="logo" name="logo" />
+      <nav className="nav-bar">
+        <input
+          type="checkbox"
+          id="toggle"
+          checked={isChecked}
+          onChange={handleToggleClick}
+          className="toggle-switch"
+        />
+
+        <label
+          htmlFor="toggle"
+          className="toggle-container"
+          onClick={handleToggleClick} 
+        >
+          <span className="icon icon-left">
+            <SvgSelector name={isChecked ? "cubeBlured" : "cube"} />
+          </span>
+          <div className={`toggle-dot ${isChecked ? "checked" : ""}`}></div> 
+          <span className="icon icon-right">
+            <SvgSelector name={isChecked ? "phone" : "phoneBlured"} />
+          </span>
+        </label>
+
+        <button className="theme-button">
+          <SvgSelector name="themeBtnIcon" />
+        </button>
+      </nav>
+    </header>
+  );
 };
-
 
 export default Header;
