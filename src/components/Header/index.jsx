94e10b6b8fbd14272bbefa2d986fbbd7d96cb589
Zhm1603 <zhanel.marat2006@gmail.com>
2025-04-27 17:03:43 +0600

    theme button styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,44 +8,53 @@ const Header = () => {
   const location = useLocation();
 
   const [isChecked, setIsChecked] = useState(location.pathname === "/users");
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  const handleThemeButtonClick = () => {
+    setIsButtonClicked((prev) => !prev); 
+  };
 
   useEffect(() => {
     navigatePages(isChecked ? "/users" : "/posts");
   }, [isChecked, navigatePages]);
 
-  const handleToggleClick = () => {
-    setIsChecked((prev) => !prev);
-  };
-
   return (
     <header className="header-vertical">
-      <SvgSelector className="logo" name="logo" />
+      <div className="logo">
+        <SvgSelector  
+          name="logo" />
+      </div>
       <nav className="nav-bar">
+        <div>
         <input
           type="checkbox"
           id="toggle"
           checked={isChecked}
-          onChange={handleToggleClick}
+          onChange={() => setIsChecked(!isChecked)}
           className="toggle-switch"
         />
 
         <label
           htmlFor="toggle"
           className="toggle-container"
-          onClick={handleToggleClick} 
         >
           <span className="icon icon-left">
             <SvgSelector name={isChecked ? "cubeBlured" : "cube"} />
           </span>
-          <div className={`toggle-dot ${isChecked ? "checked" : ""}`}></div> 
+          <div className={`toggle-dot ${isChecked ? "checked" : ""}`}></div>
           <span className="icon icon-right">
             <SvgSelector name={isChecked ? "phone" : "phoneBlured"} />
           </span>
         </label>
 
-        <button className="theme-button">
-          <SvgSelector name="themeBtnIcon" />
+        </div>
+        <div className="button-container">
+        <button className={`theme-button ${isButtonClicked ? "theme-button-clicked" : ""}`}
+          onClick={handleThemeButtonClick}>
+          <SvgSelector name={isButtonClicked ? "themeBtnIconDark" :"themeBtnIcon"} />
         </button>
+
+        </div>
       </nav>
     </header>
   );
